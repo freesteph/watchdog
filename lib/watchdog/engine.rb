1b2670b0ffb9ad931fa7d15123659024984536ad
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-04-16 18:03:19 +0100

    fix: initialise the widgets ourselves

--- lib/watchdog/engine.rb
+++ lib/watchdog/engine.rb
@@ -1,9 +1,24 @@
 module Watchdog
+  WIDGETS_CONF_PATH = 'config/widgets.yml'.freeze
+
   class Engine < ::Rails::Engine
     isolate_namespace Watchdog
 
     initializer "watchdog.assets.precompile" do |app|
       app.config.assets.precompile += %w(config/watchdog_manifest.js)
+    end
+
+    initializer "watchdog.widgets.load" do |app|
+      path = File.join(Rails.root, WIDGETS_CONF_PATH)
+
+      if File.exist? path
+        config = YAML.safe_load File.read(path)
+      else
+        warn("You haven't declared any widgets in config/widgets.yml")
+        config = { widgets: [] }
+      end
+
+      Rails.application.config.widgets = config
     end
   end
 end
