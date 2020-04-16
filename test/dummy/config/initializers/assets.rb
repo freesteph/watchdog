346075a8826dbe84f04ada8431b0941a47fa7770
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-04-16 18:14:12 +0100

    fix: space around the flash and fix the dummy app

--- test/dummy/config/initializers/assets.rb
+++ test/dummy/config/initializers/assets.rb
@@ -5,6 +5,17 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails
+  .application
+  .config
+  .assets
+  .paths
+  .concat(
+    [
+      Rails.root.join("node_modules/govuk-frontend/govuk"),
+      Rails.root.join("node_modules/govuk-frontend/govuk/assets/fonts")
+    ]
+  )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
